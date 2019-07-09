c691c4519f7c737fa88c981c01c684a60c6a2b26
Gokulapriya N <52338786+Gokulapriya98@users.noreply.github.com>
2019-07-09 10:00:12 +0530

    Add files via upload

--- newcon.py
+++ newcon.py
@@ -1,11 +1,11 @@
 import mysql.connector
 cnx = mysql.connector.connect(user='admin', password='admin123',
-                              host='127.0.0.1',
+                              host='127.0.0.1',database='priya10',
                               auth_plugin='mysql_native_password')
 
 print(cnx)
 mycursor=cnx.cursor()
-mycursor.execute("create database priya10")
-mycursor.execute("show databases")
+mycursor.execute("CREATE TABLE students (name VARCHAR(255), rollno VARCHAR(255),DOB date,Gender varchar(255),Department varchar(255))")
+mycursor.execute("SHOW TABLES")
 for x in mycursor:
     print(x)
